bf3c63da26a8f2f6e1985487c05650fef1fba52a
Kunal Kishor <kunal.kishor@mindtree.com>
2022-12-08 11:42:58 +0000

    update

--- encrypt_file.sh
+++ encrypt_file.sh
@@ -1,21 +1,20 @@
 event_bucket=$1
 event_file_name=$2
 
-decrypt_input_path=`jq -r ".decrypt_input_path" config.json`
-decrypt_output_path=`jq -r ".decrypt_output_path" config.json`
-encrypt_input_path=`jq -r ".encrypt_input_path" config.json`
-encrypt_output_path=`jq -r ".encrypt_output_path" config.json`
-
-
-echo $decrypt_input_path
-echo $decrypt_output_path
-echo $encrypt_input_path
-echo $encrypt_output_path
-
-echo $event_bucket
-echo $event_file_name
-#gcloud secrets versions access "latest" --secret="loreal-public" --out-file="test_pub.key"
+#Generate the Key to Local
+gcloud secrets versions access "latest" --secret="loreal-public" --out-file="loreal_pub.key"
 #cat test_pub.key
 #gsutil ls gs://poc-bucket-gcc-2/
 
-gpg --import /priv/loreal-private
+#copy the uploaded file to local
+
+gsutil cp gs://$event_bucket/inbound/work/$event_file_name .
+
+#Import the Public Key
+gpg --import loreal_pub.key
+#Encrypt the file
+gpg --output $event_file_name.asc --always-trust --encrypt --recipient 'sharanpreet.surjit@loreal.com' $event_file_name
+#Push the file to Target GCS
+gsutil cp $event_file_name.asc gs://$event_bucket/inbound/final/
+
+rm loreal_pub.key $event_file_name $event_file_name.asc
